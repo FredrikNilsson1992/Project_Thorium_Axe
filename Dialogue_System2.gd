1c1f44ec039b351234ef5fdd47629fcd9d07bae0
Spel 2 <h19frni@axevalla.fhsk.se>
2020-03-17 12:16:35 +0100

    Dialog fixad v.2

--- Dialogue_System2.gd
+++ Dialogue_System2.gd
@@ -6,30 +6,31 @@ class_name DialogueAction
 var dia_index = 1
 onready var player_movement = get_node("../../../Player")
 onready var text_print = get_node("RichTextLabel")
-var dialogue : Dictionary = load_dialogue(dialogue_file_path)
 
 export (String, FILE, "*.json") var dialogue_file_path : String
 
-
 func interact() -> void:
-	if dialogue.size() > 1:
+	var dialogue : Dictionary = load_dialogue(dialogue_file_path)
+	var dialogue_list = dialogue["dialogue"]
+	
+	if dialogue_list.size() > 1:
 		text_print.visible == false
 		player_movement.Move_Speed = 0
 		text_print.show()
 		
-		if dia_index != dialogue.size() + 1:
-			text_print.text = dialogue[str(dia_index)]["text"]
+		if dia_index != dialogue_list.size() - 1:
+			text_print.text = dialogue_list[dia_index]["text"]
 			dia_index += 1
 		else:
 			dia_index = 1
 			text_print.hide()
 			player_movement.Move_Speed = 5 * 65
 	
-	elif dialogue.size() == 1:
+	elif dialogue_list.size() == 1:
 		text_print.visible == false
 		player_movement.Move_Speed = 0
 		text_print.show()
-		text_print.text = dialogue["1"]["text"]
+		text_print.text = dialogue_list[1]["text"]
 		
 	else:
 		text_print.hide()
